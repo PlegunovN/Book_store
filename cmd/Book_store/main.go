e6bde3729a2e93b05ffd5a15266354cf2608609a
PkegonovN <plegunovn@gmail.com>
2024-07-01 18:09:48 +0300

    config0.1

--- cmd/Book_store/main.go
+++ cmd/Book_store/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"Book_store/configs"
 	"Book_store/internal/books"
 	"Book_store/internal/server"
+	"fmt"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 // авторизация
@@ -11,7 +15,15 @@ import (
 
 func main() {
 	//config init
+	cfg, err := configs.LoadConfig("./.env")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DbName, cfg.SslMode))
+	if err != nil {
+		log.Fatal("not connected to db")
+	}
 	storage := books.New(db)
 	server.ServerStart(storage)
 	//defer db.Close()
--- configs/config.go
+++ configs/config.go
@@ -1,43 +1,39 @@
 package configs
 
 import (
-	"errors"
 	"github.com/spf13/viper"
 	"log"
 )
 
-type localConfig struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-	DbName   string `json:"dbname"`
-	SslMode  string `json:"sslmode"`
+type Postgres struct {
+	Host     string `mapstructure:"DB_host"`
+	Port     string `mapstructure:"DB_port"`
+	User     string `mapstructure:"DB_user"`
+	Password string `mapstructure:"DB_password"`
+	DbName   string `mapstructure:"DB_name"`
+	SslMode  string `mapstructure:"DB_sslmode"`
 }
 
-func LoadConfig(fileName string, fileType string) (*viper.Viper, error) {
-	v := viper.New()
-	v.SetConfigType(fileType)
-	v.SetConfigName(fileName)
-	v.AddConfigPath("./configs")
-	v.AutomaticEnv()
+func LoadConfig(path string) (cfg *Postgres, err error) {
 
-	err := v.ReadInConfig()
+	cfg = new(Postgres)
+
+	viper.SetConfigFile(path)
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
 	if err != nil {
-		log.Printf("Unable to read config: %v", err)
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			return nil, errors.New("config file not found")
-		}
-		return nil, err
+		log.Println("Read config error")
+		return
 	}
-	return v, nil
+
+	err = viper.Unmarshal(cfg)
+	if err != nil {
+		log.Println("Unmarshal config error")
+		return
+	}
+
+	return cfg, nil
 }
-
-//1
-//db, err := sqlx.Connect("postgres", v.ConfigFileUsed())
-
-//2
-//db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-//	v.Get("host"), v.Get("port"), v.Get("user"), v.Get("password"), v.Get("dbname"), v.Get("sslmode")))
-
-//db, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=1234 dbname=test_books sslmode=disable")
